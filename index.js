fe90da8a35104a32d892f6957c05d33f8d6521e3
Conrad Ohlsson <lusffs@gmail.com>
2019-01-24 15:07:43 +0100

    Initial version of the Mocked API

---
+++ index.js
@@ -0,0 +1,52 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+var walk = require('walk');
+
+require('dotenv').config()
+
+// Walker options
+var walker  = walk.walk('./api', { followLinks: false });
+var files = [];
+
+walker.on('file', function(root, stat, next) {
+    // Add this file to the list of files
+    files.push(root + '/' + stat.name);
+    next();
+});
+
+walker.on('end', function() {
+  console.log(`- - - - - - - - - - - - - - - - - - - - - - - - - `)
+  console.log("");
+  console.log(`🐙 Great API Mock!`)
+  console.log("");
+  console.log(`- - - - - - - - - - - - - - - - - - - - - - - - - `)
+  console.log(`Available endpoints:`)
+
+  for (let i = 0; i < files.length; i++) {
+    let filePath = files[i];
+    filePath = filePath.replace("/response.json", "");
+    filePath = filePath.replace("./", "/");
+
+    app.get(filePath, (req, res) => {
+      console.log(`Request: ${req.route.path}`);
+      res.header("Content-Type",'application/json');
+      res.header("Access-Control-Allow-Origin", process.env.ACCESS_CONTROL_ALLOW_ORIGIN)
+      res.header("Access-Control-Allow-Credentials", "true")
+      res.header("Status", 200)
+      res.sendFile(path.join(__dirname, filePath, "response.json"));
+    })
+  }
+
+  for (let i = 0; i < app._router.stack.length; i++) {
+    const r = app._router.stack[i];
+    if(r.route && r.route.path){
+        var methods = r.route.methods.get ? "[GET]" : "";
+        console.log(`${methods} http://localhost:${process.env.PORT}${r.route.path}`)
+    }      
+  }
+console.log(`- - - - - - - - - - - - - - - - - - - - - - - - - `)
+  // Start the server
+  app.listen(process.env.PORT)
+  console.log(`Server is up and running on http://localhost:${process.env.PORT}`)
+});
